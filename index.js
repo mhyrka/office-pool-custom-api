b7e8a5b3fe130678b348e85700c997008498c40c
Matthew Hyrka <mhyrka@gmail.com>
2018-05-18 15:22:01 -0600

    debugging

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const port = parseInt(process.env.PORT || 3000)
 const data = require('./app')
 
