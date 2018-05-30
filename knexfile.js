6db73e9a41ae88ee7341842aa809d1859b9141a9
Matthew Hyrka <mhyrka@gmail.com>
2018-05-30 15:38:09 -0600

    configuring heroku db

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,6 @@ module.exports = {
   test: {},
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: 'postgres://lwszogzwaqfsil:16cfe93c2101800fbb7b344c347b1d47273c4aabf24bfffc4ec571a985075e53@ec2-107-20-133-82.compute-1.amazonaws.com:5432/dcmjr80gmgutcs'
   }
 }
